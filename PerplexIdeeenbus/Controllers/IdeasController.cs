17e115b152a10c7b0e64cf199f13fe1bd47cf13e
Sd8991 <sjors.derksen@outlook.com>
2025-04-07 16:04:32 +0200

    Text voor bestaande error messages, pseudocode voor datumvereiste van uitjes, bug fixes, clean-up,

--- PerplexIdeeenbus/Controllers/IdeasController.cs
+++ PerplexIdeeenbus/Controllers/IdeasController.cs
@@ -29,13 +29,13 @@ namespace PerplexIdeeenbus.Controllers
 
         // GET: api/Ideas/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Idea>> GetIdea(int id)
+        public async Task<ActionResult<Idea>> GetIdea(long id)
         {
             var idea = await _context.Ideas.FindAsync(id);
 
             if (idea == null)
             {
-                return NotFound();
+                return NotFound("Entry with Id " + id + " does not exist.");
             }
 
             return idea;
@@ -44,11 +44,16 @@ namespace PerplexIdeeenbus.Controllers
         // PUT: api/Ideas/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutIdea(int id, Idea idea)
+        public async Task<IActionResult> PutIdea(long id, Idea idea)
         {
+            if (!IdeaExists(id))
+            {
+                return NotFound("Entry with Id " + id + " does not exist.");
+            }
+
             if (id != idea.Id)
             {
-                return BadRequest();
+                return BadRequest("Cannot change an existing Id");
             }
 
             _context.Entry(idea).State = EntityState.Modified;
@@ -61,7 +66,7 @@ namespace PerplexIdeeenbus.Controllers
             {
                 if (!IdeaExists(id))
                 {
-                    return NotFound();
+                    return NotFound("Entry with Id " + id + " does not exist.");
                 }
                 else
                 {
@@ -77,6 +82,15 @@ namespace PerplexIdeeenbus.Controllers
         [HttpPost]
         public async Task<ActionResult<Idea>> PostIdea(Idea idea)
         {
+            /* Pseudocode idee voor datumvereiste. Momenteel wordt bij afwezigheid van begin- en einddatum in het request een default waarde van '0001-01-01 00:00' toegevoegd.
+             * Om het onderstaande goed the implementeren zou het daarom ideaal zijn als de begin- en einddatum nullable zijn, maar deze aanpassing is mij niet op tijd gelukt.
+             *
+             * if (idea.Type == IdeaType.uitje)
+             *      if (idea.BeginDatum == null)
+             *          return BadRequest("BeginDatum required for Type uitje");
+             *      if (idea.EindDatum == null)
+             *          return BadRequest("EindDatum required for Type uitje");           
+             */
             _context.Ideas.Add(idea);
             await _context.SaveChangesAsync();
 
@@ -85,12 +99,12 @@ namespace PerplexIdeeenbus.Controllers
 
         // DELETE: api/Ideas/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteIdea(int id)
+        public async Task<IActionResult> DeleteIdea(long id)
         {
             var idea = await _context.Ideas.FindAsync(id);
             if (idea == null)
             {
-                return NotFound();
+                return NotFound("Entry with Id " + id + " does not exist.");
             }
 
             _context.Ideas.Remove(idea);
@@ -99,7 +113,7 @@ namespace PerplexIdeeenbus.Controllers
             return NoContent();
         }
 
-        private bool IdeaExists(int id)
+        private bool IdeaExists(long id)
         {
             return _context.Ideas.Any(e => e.Id == id);
         }
--- PerplexIdeeenbus/Models/Idea.cs
+++ PerplexIdeeenbus/Models/Idea.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using ExpressiveAnnotations.Attributes;
 
 namespace PerplexIdeeenbus.Models
 {
@@ -7,9 +6,10 @@ namespace PerplexIdeeenbus.Models
 
     public class Idea
     {
-        public long Id { get; }
+        // Het liefst zou ik het Id uit het inputformat laten (ook al is het nu optioneel), maar het verwijderen van de setter breekt PUT requests
+        public long Id { get; set; }
 
-        /*Mogelijke verbetering: DateTime.Now is niet bruikbaar op deze manier. Momenteel wordt the CreationTime aangepast bij elke GET van een Idea.
+        /*Mogelijke verbetering: DateTime.Now is niet bruikbaar op deze manier. Momenteel wordt de CreationTime aangepast bij elke GET request
          De huidige implementatie sorteert de data in site.js op het Id van de Ideas, in plaats van de CreationTime. Dit zou tot hetzelfde resultaat moeten leiden als een correcte sort op de CreationTime, 
          aangezien Ids van verwijderde Ideas niet opnieuw gebruikt worden. Deze aanpak is echter wel beperkt door de maximale waarde van het Id. */
         public DateTime CreationTime { get; } = DateTime.Now; 
@@ -34,13 +34,13 @@ namespace PerplexIdeeenbus.Models
             }
             set
             {
-                _ideaType = (IdeaType)Enum.Parse(typeof(IdeaType), value); //TODO: niet crashen op ongeldig type
+                _ideaType = (IdeaType)Enum.Parse(typeof(IdeaType), value); //Verbetering: niet crashen op ongeldig type
             }
         }
 
         private DateTime _beginDatum;
 
-        [RequiredIf("_ideaType == IdeaType.uitje")] //TODO: Dit lijkt niet afgedwongen te worden. Zoek uit waarom.
+        // Mogelijke verbetering: Het liefst zou ik de begin- en einddatumvereiste voor uitjes op een soortgelijke manier afdwingen als de andere vereiste delen, maar dit is niet op tijd gelukt.
         public string BeginDatum {
             get 
             { 
@@ -48,13 +48,12 @@ namespace PerplexIdeeenbus.Models
             }
             set 
             {
-                _beginDatum = DateTime.ParseExact(value, "yyyy-MM-dd HH:mm", null); //TODO: Niet crashen op ongeldig format
+                _beginDatum = DateTime.ParseExact(value, "yyyy-MM-dd HH:mm", null); //Verbetering: Niet crashen op ongeldig format
             } 
         }
 
         private DateTime _eindDatum;
 
-        [RequiredIf("_ideaType == IdeaType.uitje")]
         public string EindDatum {
             get
             {
@@ -62,14 +61,11 @@ namespace PerplexIdeeenbus.Models
             }
             set
             {
-                _eindDatum = DateTime.ParseExact(value, "yyyy-MM-dd HH:mm", null); //TODO: Niet crashen op ongeldig format
+                _eindDatum = DateTime.ParseExact(value, "yyyy-MM-dd HH:mm", null); //Verbetering: Niet crashen op ongeldig format
             }
         }
 
         public TimeSpan Duur { get { return _eindDatum.Subtract(_beginDatum); } } 
-
-        //public DateTime BeginDatum { get; set; }
-        //public DateTime EindDatum { get; set; }
 
         public List<string>? Categories { get; set; }
     }
